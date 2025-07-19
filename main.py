fba8272a75c755557c736bf781114a3d60fcac9e
carolinemillan <carolinemillan5678@gmail.com>
2025-07-19 18:02:19 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # fill screen with black
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
